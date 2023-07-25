3f02c8d10618e3e091be67fcd9638c1656d1825d
AntonelloStefania <antonelloste94@gmail.com>
2023-07-25 16:23:33 +0200

    [fix] posizione immagine comics.blade

--- resources/views/comics/comics.blade.php
+++ resources/views/comics/comics.blade.php
@@ -5,15 +5,15 @@
     @include('partials.jumbotron')
         <div class="col-12 blue-line-comics"></div>
         <div class="d-flex justify-content-around position-relative">
-            <div class="col-3 ms-5 film-card d-flex position-absolute card-show " style="background-image:url({{ $comic ->thumb }})"
+            <div class="col-3  d-flex position-absolute card-show " style="background-image:url({{ $comic ->thumb }})"
                 alt="poster">
                 <div class="w-100 d-flex align-self-end h-100 flex-column justify-content-between">
                 <span class="label w-50 text-center text-thumb">COMIC BOOK</span>
                 <span class="label w-100 text-center text-thumb">VIEW GALLERY</span>
                 </div>
             </div>
-            <div  class="text-white ps-5 py-4">
-                <div class="d-flex justify-content-around pt-5">
+            <div  class="text-white ps-5 py-4 d-flex justify-content-center">
+                <div class="d-flex justify-content-around pt-5 w-75">
                     <div class="col">
                         <h4>{{$comic->title}}</h4>
                         <h6>{{$comic->series}}</h6>
